3866905a0e3bb1a4af0ef085cab3283b47a5a5ad
milenimodesto <140817914+milenimodesto@users.noreply.github.com>
2024-07-02 09:58:36 -0300

    Add files via upload

---
+++ 2-media-forEach.js
@@ -0,0 +1,4 @@
+const notas = [10, 6.5, 8, 7.5];
+
+//o forEach é um método e dentro do parâmetro dele
+//passaremos uma outra função para esse método.
---
+++ desafionotas.js
@@ -0,0 +1,5 @@
+const notas = [8.7, 6.5, 7.0, 8.0 ]
+for (let i=0; i < notas.length; i++){
+    notas[1] = notas[i] + 1
+}
+console.log(notas)
