ca47271afe71e6a7cf7a31d7e1f3704e6df770b3
milenimodesto <140817914+milenimodesto@users.noreply.github.com>
2024-07-02 11:04:51 -0300

    Add files via upload

--- 2-media-forEach.js
+++ 2-media-forEach.js
@@ -1,4 +1,5 @@
 const notas = [10, 6.5, 8, 7.5];
 
-//o forEach é um método e dentro do parâmetro dele
-//passaremos uma outra função para esse método.
+notas.forEach(function(){
+    console.log(notas)
+})
---
+++ 4-media-forEach.js
@@ -0,0 +1,7 @@
+const notas =[10, 6.5, 8, 7.5]
+let somaDasNotas = 0;
+notas.forEach(function(notas){
+    somaDasNotas + = notas;
+});
+const media = somaDasNotas / notas.length;
+console.log( ` a média das notas é ${media}.`)
