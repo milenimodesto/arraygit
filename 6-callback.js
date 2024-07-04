d5c07fe6b8ae0e9e4a9d353377623fbdb041bd4c
milenimodesto <140817914+milenimodesto@users.noreply.github.com>
2024-07-04 11:25:21 -0300

    Add files via upload

---
+++ 6-callback.js
@@ -0,0 +1,14 @@
+const nomes =[ " Evaldo","Mari","Casis"];
+nomes.forEach(function(nome){
+
+})
+nomes.forEach((nome)=>{
+console.log(nome)
+
+})
+nomes.forEach(function(){})
+nomes.forEach(()=>{})
+function imprimenNome(nome){
+    console.log(nome);
+}
+nomes.forEach(imprimenNome);
---
+++ 7.for_especifico.js
@@ -0,0 +1,5 @@
+const lista = [1,2,3,4,5];
+
+for (let i = 0, j = 0 ; i < lista.length; i++, j++) {
+    
+}
