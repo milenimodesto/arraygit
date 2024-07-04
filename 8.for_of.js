93dcabf8193cd93b404ff0e8930aa8c9198cbca6
milenimodesto <140817914+milenimodesto@users.noreply.github.com>
2024-07-04 11:53:45 -0300

    Add files via upload

---
+++ 8.for_of.js
@@ -0,0 +1,8 @@
+const lista = [1, 2, 3, 4, 5];
+let some = 0; 
+
+
+for( let elemento of lista) {
+    soma += elemento;
+}
+console.log(soma)
---
+++ 9.forEach.js
@@ -0,0 +1,10 @@
+const lista = [1, 2, 3, 4, 5];
+let soma = 0;
+
+lista.forEach(numero => soma += numero);
+console.log(soma)
+
+lista.forEach(numero => {
+    soma = soma + numero
+});
+console.log(soma)
